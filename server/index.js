225550f5265658260791bfd6880c811927b8d79f
chao <dc10101@qq.com>
2018-01-08 10:41:43 +0800

    log

--- server/index.js
+++ server/index.js
@@ -8,18 +8,22 @@ const clientDirectUploadController = require('./controllers/client-direct-upload
 const app = new Koa()
 const port = 2018
 
+app.use(cors({ credentials: true }))
+app.use(bodyParser())
+let counter = 0
 app.use(async (ctx, next) => {
+	counter++
 	try{
+		let requestId = counter
+		console.log(requestId, ctx.method, ctx.url, ctx.headers, ctx.rawBody)
 		await next()
+		console.log(requestId, ctx.body)
 	}
 	catch(e){
 		console.error(e)
 		ctx.body = e.message
 	}
 })
-
-app.use(cors({ credentials: true }))
-app.use(bodyParser())
 
 router.get('/signature', clientDirectUploadController.getSignature)
 router.post('/upload-callback', clientDirectUploadController.uploadCallback)
